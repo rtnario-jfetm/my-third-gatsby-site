cb5d079b16bb47e8f13a96321364e38143598423
Ramon Roberto T. Nario <ramon-nario@tm.jfe-eng.co.jp>
2024-01-11 14:14:50 +0800

    feat: experiment with children

---
+++ src/components/frame.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+
+const Frame = ({ children }) => {
+  return (
+    <div>
+      <p>This would be a picture, but alas, it is not.</p>
+      {children}
+    </div>
+  )
+}
+
+export default Frame
